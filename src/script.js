fe48e66428c33deb46e796579eb8bfa5f4ebc521
rahmatsyifana12 <adean435@gmail.com>
2022-05-03 22:09:53 +0700

    Update color and add animation

---
+++ src/script.js
@@ -0,0 +1,38 @@
+// Home
+$(window).on('load', function() {
+    $('#home .container').addClass('show');
+    let wScroll = $(this).scrollTop();
+
+    console.log(wScroll);
+    if (wScroll > $('#about').offset().top - 546) {
+        $('#about .title').addClass('show');
+        setTimeout(() => {
+            $('#about .about-img').addClass('show');
+        }, 300);
+        setTimeout(() => {
+            $('#about .about-me-title').addClass('show');
+        }, 300);
+        setTimeout(() => {
+            $('#about .desc').addClass('show');
+        }, 300);
+    }
+});
+
+$(window).scroll(function() {
+    let wScroll = $(this).scrollTop();
+
+    // About Me
+    if (wScroll > $('#about').offset().top - 546) {
+        $('#about .title').addClass('show');
+        setTimeout(() => {
+            $('#about .about-img').addClass('show');
+        }, 300);
+        setTimeout(() => {
+            $('#about .about-me-title').addClass('show');
+        }, 300);
+        setTimeout(() => {
+            $('#about .desc').addClass('show');
+        }, 300);
+    }
+});
+
